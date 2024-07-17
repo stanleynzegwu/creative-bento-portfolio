e4bd8c466db4f6da3732e2dbda41fc8d3187f391
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-07-17 17:25:00 +0100

    added vercel.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import Topbar from "./components/Topbar";
 
 function App() {
   return (
-    <main className="max-md:mt-16">
-      <Topbar />
-      {/* Defining routes path and rendering components as element */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </main>
+    <Router>
+      <main className="max-md:mt-16">
+        <Topbar />
+        {/* Defining routes path and rendering components as element */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </Router>
   );
 }
 
