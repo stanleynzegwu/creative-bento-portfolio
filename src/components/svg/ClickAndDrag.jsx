f1152e111783e00be7a867d19084fce18ac78a47
Stanley Nzegwu <nzegwustanley@gmail.com>
2024-07-29 08:15:27 +0100

    new commit

--- src/components/svg/ClickAndDrag.jsx
+++ src/components/svg/ClickAndDrag.jsx
@@ -1,11 +1,12 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const ClickAndDrag = () => {
   const leftArrowRef = useRef(null);
   const rightArrowRef = useRef(null);
 
-  useEffect(() => {
+  useGSAP(() => {
     const timeline = gsap.timeline({ repeat: -1, yoyo: true });
 
     timeline
@@ -22,30 +23,30 @@ const ClickAndDrag = () => {
       fill="#000000"
       className="absolute left-1/2 bottom-0 -translate-x-1/2 w-16 h-16 pointer-events-none z-20"
     >
-      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-      <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+      <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
       <g id="SVGRepo_iconCarrier">
         <path
           d="M13,22.5,7.82,17.36a2,2,0,0,1-.59-1.43,2,2,0,0,1,2-2,2,2,0,0,1,1.43.59L12,15.82V6.38a2,2,0,0,1,1.74-2,1.87,1.87,0,0,1,1.51.56,1.83,1.83,0,0,1,.57,1.34V12l5,.72a1.91,1.91,0,0,1,1.64,1.89h0a17.18,17.18,0,0,1-1.82,7.71l-.09.18"
           fill="none"
           stroke="#FFFFFF"
-          stroke-miterlimit="10"
-          stroke-width="1.91"
+          strokeMiterlimit="10"
+          strokeWidth="1.91"
         />
         <path
           d="M15.82,10.64a4.54,4.54,0,0,0,1.47-1,4.78,4.78,0,1,0-6.76,0,4.54,4.54,0,0,0,1.47,1"
           fill="none"
           stroke="#FFFFFF"
-          stroke-miterlimit="10"
-          stroke-width="1.91"
+          strokeMiterlimit="10"
+          strokeWidth="1.91"
         />
         <polyline
           ref={leftArrowRef}
           points="4.36 9.14 1.5 6.27 4.36 3.41"
           fill="none"
           stroke="#FFFFFF"
-          stroke-miterlimit="10"
-          stroke-width="1.91"
+          strokeMiterlimit="10"
+          strokeWidth="1.91"
         />
         <line
           ref={leftArrowRef}
@@ -55,16 +56,16 @@ const ClickAndDrag = () => {
           y2="6.27"
           fill="none"
           stroke="#FFFFFF"
-          stroke-miterlimit="10"
-          stroke-width="1.91"
+          strokeMiterlimit="10"
+          strokeWidth="1.91"
         />
         <polyline
           ref={rightArrowRef}
           points="27.64 9.14 30.5 6.27 27.64 3.41"
           fill="none"
           stroke="#FFFFFF"
-          stroke-miterlimit="10"
-          stroke-width="1.91"
+          strokeMiterlimit="10"
+          strokeWidth="1.91"
         />
         <line
           ref={rightArrowRef}
@@ -74,8 +75,8 @@ const ClickAndDrag = () => {
           y2="6.27"
           fill="none"
           stroke="#FFFFFF"
-          stroke-miterlimit="10"
-          stroke-width="1.91"
+          strokeMiterlimit="10"
+          strokeWidth="1.91"
         />
       </g>
     </svg>
--- src/pages/IdeaBreakdown.jsx
+++ src/pages/IdeaBreakdown.jsx
@@ -49,38 +49,80 @@
 
 // export default IdeaBreakdown;
 
+/////////////////////////
+
 import { IDEA_DATA } from "@/constants";
 import { useNavigate, useParams } from "react-router-dom";
+import gsap from "gsap";
+import { useRef, useState } from "react";
 import View360Experience from "@/canvas/View360";
 import ClickAndDrag from "@/components/svg/ClickAndDrag";
+import { useGSAP } from "@gsap/react";
 
 const IdeaBreakdown = () => {
-  return (
-    <div className="relative h-full w-full">
-      <Button />
-      <ClickAndDrag />
-      <View360Experience textureUrl={"/textures/ideas_texture2.png"} />
-    </div>
-  );
-};
-
-export default IdeaBreakdown;
-
-const Button = () => {
+  const [isView360, setView360] = useState(false);
   const navigate = useNavigate();
-  // const { id } = useParams();
-  // const idea = IDEA_DATA.find((_, index) => index === +id);
+  const { id } = useParams();
+  const idea = IDEA_DATA.find((_, index) => index === +id);
 
   const handleGoBack = () => {
     navigate(-1);
   };
 
+  const elementRef = useRef(null);
+  useGSAP(() => {
+    // Animation setup
+    const animation = gsap.fromTo(
+      elementRef.current,
+      { y: "20%", opacity: 0 },
+      { y: "0%", opacity: 1, duration: 0.5, ease: "power2.out" }
+    );
+    // Cleanup function to stop animation on component unmount
+    return () => {
+      animation.kill();
+    };
+  }, []);
+
   return (
-    <button
-      className={`absolute top-5 left-5 font-medium  text-sm bg-black text-white rounded-full px-4 py-1 transition-all duration-400 ease-in-out hover:scale-110 z-20`}
-      onClick={handleGoBack}
-    >
-      BACK
-    </button>
+    <>
+      {!isView360 ? (
+        <div className="p-8 lg:p-12 py-10" ref={elementRef}>
+          <div className="text-sm text-gray-400 cursor-pointer mb-14" onClick={handleGoBack}>
+            Back
+          </div>
+
+          <div className="flex flex-col gap-8">
+            <div className="flex justify-between ">
+              <h2 className="capitalize text-xl font-semibold">{idea.header}</h2>
+              <span className="text-sm text-gray-400">{idea.date}</span>
+            </div>
+            <p className="text-sm ">{idea.desc}</p>
+            <div
+              className="border-[1px] border-gray-300 bg-gray-100 rounded-md h-96 cursor-pointer"
+              onClick={() => setView360(true)}
+            >
+              <img
+                src="/textures/ideas_texture2.png"
+                alt="360view_preview"
+                className="w-full h-full rounded-md"
+              />
+            </div>
+          </div>
+        </div>
+      ) : (
+        <div className="relative h-full w-full">
+          <button
+            className={`absolute top-5 left-5 font-medium  text-sm bg-black text-white rounded-full px-4 py-1 transition-all duration-400 ease-in-out hover:scale-110 z-20`}
+            onClick={() => setView360(false)}
+          >
+            BACK
+          </button>
+          <ClickAndDrag />
+          <View360Experience textureUrl={"/textures/ideas_texture2.png"} />
+        </div>
+      )}
+    </>
   );
 };
+
+export default IdeaBreakdown;
